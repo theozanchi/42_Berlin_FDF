5942f204352c6ef24863ee2560a6592b15c2b126
tzanchi <tzanchi@student.42berlin.de>
2023-07-12 18:07:08 +0200

    enum e_bool

--- includes/fdf.h
+++ includes/fdf.h
@@ -6,7 +6,7 @@
 /*   By: tzanchi <tzanchi@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/12 10:55:44 by tzanchi           #+#    #+#             */
-/*   Updated: 2023/07/12 12:01:37 by tzanchi          ###   ########.fr       */
+/*   Updated: 2023/07/12 17:51:49 by tzanchi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,11 @@
 
 # include "../libft/libft.h"
 # include <math.h>
+
+typedef enum e_bool
+{
+	FALSE,
+	TRUE
+}	t_bool;
 
 #endif
